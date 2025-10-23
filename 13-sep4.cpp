bd06b317af9e77d058f13bf670adf685e329afd4
Eshika88 <eshikagupta505@gmail.com>
2025-10-23 20:34:25 +0530

    Add files via upload

---
+++ 13-sep4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+class solution{
+    public:
+    void rotate(vector<int>& nums, int k) {
+        int n=nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end()-k);
+        reverse(nums.end()-k,nums.end());
+        reverse(nums.begin(),nums.end());
+            
+        }
+};
