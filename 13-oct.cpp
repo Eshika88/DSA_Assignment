8f1d3e9785592dabfd213ad59d5f7e6ae5fc4348
Eshika88 <eshikagupta505@gmail.com>
2025-10-23 20:00:34 +0530

    Add files via upload

---
+++ 13-oct.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+class solution{
+    public:
+    void reverseArray(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        int i=0,j=n-1;
+        while(i<=j){
+            swap(arr[i],arr[j]);
+            i++,j--;
+        }
+        return ;
+    }
+};
