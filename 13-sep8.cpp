23d3b79d74c73733fb37715fa418d637e91a049a
Eshika88 <eshikagupta505@gmail.com>
2025-10-27 15:42:35 +0530

    Add files via upload

---
+++ 13-sep8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+class solution{
+    public:
+    void selectionSort(vector<int> &arr) {
+        // code here
+        int n=arr.size();
+        for(int i=0;i<n-1;i++){
+            int min_idx=i;
+            for(int j=i+1;j<n;j++){
+                if(arr[j]<arr[min_idx]) min_idx=j;
+            }
+            swap(arr[i],arr[min_idx]);
+        }
+    }
+};
